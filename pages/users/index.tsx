630b0b74f8920445883de635680f9b3a1975eb29
twinedo <twinedo.dev@gmail.com>
2022-02-01 17:34:09 +0700

    getstaticprops

--- pages/users/index.tsx
+++ pages/users/index.tsx
@@ -1,12 +1,35 @@
 import React from 'react';
 import Layout from '../../components/Layout';
 
-const Users = () => {
-    return (
-        <Layout pageTitle='Users'>
-            <div>User Page</div>
-        </Layout>
-    );
+interface UserProps {
+  dataUsers: Array<any>;
+}
+
+const Users = ({ dataUsers }: UserProps) => {
+  console.log('dataUsers', dataUsers);
+  return (
+    <Layout pageTitle='Users'>
+      <div>User Page</div>
+      {dataUsers.map((user) => {
+        return (
+          <>
+            <p>{user.name}</p>
+            <p>{user.email}</p>
+          </>
+        );
+      })}
+    </Layout>
+  );
 };
 
 export default Users;
+
+export async function getStaticProps() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const dataUsers = await res.json();
+  return {
+    props: {
+      dataUsers,
+    },
+  };
+}
