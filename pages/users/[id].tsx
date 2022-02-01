8dac0410c3693f0e107577dbca240972327edd88
twinedo <twinedo.dev@gmail.com>
2022-02-01 18:08:37 +0700

    getstaticprops dan getstaticpaths

--- pages/users/[id].tsx
+++ pages/users/[id].tsx
@@ -1,10 +1,55 @@
 import React from 'react';
 import { useRouter } from 'next/router';
+import Layout from '../../components/Layout';
 
-function Detail() {
-    const router = useRouter();
-    const { id } = router.query;
-    return <div>User Detail {id}</div>;
+interface User {
+  id: number;
+  name: string;
+  email: string;
+  phone: string;
+  website: string;
+}
+
+interface UserDetailProps {
+  user: User;
+}
+
+function Detail({ user }: UserDetailProps) {
+  const router = useRouter();
+  const { id } = router.query;
+  return (
+    <Layout pageTitle='Detail'>
+      <p>{user.name}</p>
+      <p>{user.email}</p>
+      <p>{user.phone}</p>
+      <p> {user.website}</p>
+    </Layout>
+  );
 }
 
 export default Detail;
+
+export async function getStaticPaths() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const dataUsers = await res.json();
+  const paths = dataUsers.map((user: User) => ({
+    params: { id: `${user.id}` },
+  }));
+  return { paths, fallback: true };
+}
+
+interface GetStaticProps {
+  params: { id: string };
+}
+
+export async function getStaticProps(context: GetStaticProps) {
+  const { id } = context.params;
+  const res = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
+  const user = await res.json();
+
+  return {
+    props: {
+      user,
+    },
+  };
+}
--- pages/users/index.tsx
+++ pages/users/index.tsx
@@ -1,21 +1,28 @@
 import React from 'react';
 import Layout from '../../components/Layout';
+import { useRouter } from 'next/router';
+import styles from '../../styles/Users.module.css';
 
 interface UserProps {
   dataUsers: Array<any>;
 }
 
 const Users = ({ dataUsers }: UserProps) => {
+  const router = useRouter();
   console.log('dataUsers', dataUsers);
   return (
     <Layout pageTitle='Users'>
       <div>User Page</div>
       {dataUsers.map((user) => {
         return (
-          <>
+          <div
+            key={user.id}
+            onClick={() => router.push(`/users/${user.id}`)}
+            className={styles.card}
+          >
             <p>{user.name}</p>
             <p>{user.email}</p>
-          </>
+          </div>
         );
       })}
     </Layout>
